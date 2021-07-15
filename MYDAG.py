d1bb05fe5b61c55ce0e34a912676ed874935873d
Leander leitao <leandersavio@live.com>
2021-07-15 19:15:45 +0530

    added python operator

--- MYDAG.py
+++ MYDAG.py
@@ -1,19 +1,16 @@
 from airflow import DAG
-from airflow.operators import BashOperator
+from airflow.operators.bash_operator import BashOperator
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime, timedelta
-import requests
-import pandas as pd
-import json
-from pandas.io.json import json_normalize 
-from airflow.operators.email_operator import EmailOperator
 
-data_json=[]
+from airflow.operators.email import EmailOperator
+import papermill as pm
+
 
 default_args = {
     'owner': 'leander',
     'depends_on_past': False,
-    'start_date': datetime.today() - timedelta(days=13),
+    'start_date': datetime.today() - timedelta(days=5),
     'email': ['leandersavio@gmail.com'],
     'email_on_failure': False,
     'email_on_retry': False,
@@ -23,37 +20,26 @@ default_args = {
 
 dag = DAG('MYDAG', default_args=default_args,schedule_interval='0 11 * * *')
 
-def fetchDataAPI():
-    symbols=['NFLX','MSFT','GOOGL','FB','AMZN','BABA','TSLA']
-    
-    for val in symbols:
-        r = requests.get('https://finnhub.io/api/v1/news-sentiment?symbol='+str(val)+'&token=bvv9c7f48v6r5v93tap0')
-        data_json.append(r.json())
-        df = json_normalize(data_json)
-        df.to_csv("/usr/local/airflow/stocks.csv")
-        
 
-    
-def datatransform():
-    df=pd.read_csv("/usr/local/airflow/stocks.csv")
-    df_cols=df.iloc[:,[1,3,5,6,2,7]]
-    df_cols.columns=['NewsScore','Symbol','Buzz','WeekAvgBuzz','BearSentiment','BullSentiment']
-    df_cols=df_cols[['Symbol','NewsScore','Buzz','WeekAvgBuzz','BearSentiment','BullSentiment']]
-    df_final=df_cols.sort_values(by=['BullSentiment'],ascending=False).reset_index()
+ 
 
+def DataFetch_Transform_Clean():
+    pm.execute_notebook(
+    '/mnt/c/users/leand/AirflowHome/dags/stock.ipynb',
+    '/mnt/c/users/leand/AirflowHome/dags/stock_out.ipynb',
+    parameters=dict(alpha=0.6, ratio=0.1)
+     
+)
 
-
-fetchDataAPI = PythonOperator(
-        task_id="fetch_data_from_API",
-        python_callable=fetchDataAPI,
+DataFetch_Transform_Clean = PythonOperator(
+        task_id="run_notebook",
+        python_callable=DataFetch_Transform_Clean,
         dag=dag
     )
-datatransform = PythonOperator(
-        task_id="datatransform",
-        python_callable=datatransform,
-        dag=dag
-    )
+
+
     
+
 send_email = EmailOperator(
         task_id='send_email',
         to='leandersavio@gmail.com',
@@ -64,7 +50,7 @@ send_email = EmailOperator(
 
   
     
-fetchDataAPI >> datatransform >> send_email
+DataFetch_Transform_Clean >> send_email
 
 
 
